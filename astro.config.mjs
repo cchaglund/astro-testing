95fdea3b1474e2e5e69335ff8071243ad757b825
Christofer Haglund <christofer.haglund@24hr.se>
2025-09-24 13:48:50 +0200

    netlify and solid-js adaptors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import solidJs from '@astrojs/solid-js';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [ solidJs() ],
+    adapter: netlify()
+});
